089869ed34bd1d7b52ffa75da8853cd825e772d1
SheeRMooN <sheermood@gmail.com>
2019-03-15 16:26:16 +0300

    begin

---
+++ src/Grade/Grade.java
@@ -0,0 +1,26 @@
+package Grade;
+
+public class Grade {
+    public static void main(String[] args) {
+
+        int hour = (int) (Math.random() * 12);
+        int minutes = (int) (Math.random() * 60);
+        System.out.println("Время  "+hour+"."+minutes);
+        System.out.println("градус между стрелками часов ="+grade(hour ,minutes));
+    }
+
+    public static int grade(int hour, int minutes){
+
+        int gradeH = hour*(360/12)+ minutes*(30/60);
+        int gradeM = minutes*(360/60);
+
+        if (gradeH > gradeM) {
+            return gradeH - gradeM;
+        } else if (gradeH < gradeM) {
+            return gradeM - gradeH;
+        } else
+            return  0;
+    }
+}
+
+
