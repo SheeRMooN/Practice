4f616e31ae45dd30b88a6c52fe2838b6951c81dd
SheeRMooN <sheermood@gmail.com>
2019-03-15 21:50:26 +0300

    bubble back sort

---
+++ src/LessonSort/Bubble/BubbleBack.java
@@ -0,0 +1,27 @@
+package LessonSort.Bubble;
+import java.util.Arrays;
+
+public class BubbleBack {
+    public static void main(String[] args) {
+        int[] arry = new int[10];
+
+        for (int i = 0; i < arry.length; i++)
+            arry[i] = (int) (Math.random() * 99);
+
+        System.out.println(Arrays.toString(arry));
+        System.out.println("----------------------------------------");
+
+        for (int size = 0; size < arry.length ; size++) {
+            for (int index = arry.length-1; index > size; index--) {
+                if (arry[index] > arry[index - 1]) {
+                    int a = arry[index - 1];
+                    arry[index - 1] = arry[index];
+                    arry[index] = a;
+                }
+            }
+            System.out.println(Arrays.toString(arry));
+        }
+
+        System.out.println(Arrays.toString(arry));
+    }
+}
